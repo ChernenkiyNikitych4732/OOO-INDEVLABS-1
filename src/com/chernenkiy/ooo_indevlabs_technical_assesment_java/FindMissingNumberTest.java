19beee2fe8360dbf95ee63c0e0e2ca0e9b9171b8
Nikita <n.tchernenky@yandex.ru>
2024-09-25 21:25:09 +0200

    Техническое задание для компании ООО ИНДЕВЛАБС - 1

--- src/com/chernenkiy/ooo_indevlabs_technical_assesment_java/FindMissingNumberTest.java
+++ src/com/chernenkiy/ooo_indevlabs_technical_assesment_java/FindMissingNumberTest.java
@@ -6,14 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FindMissingNumberTest {
     @Test
     public void testFindMissingNumber_FirstArray() {
-        int num[] = {5,0,1,3,2};
+        int[] num = {5,0,1,3,2};
         int missingNumber = FindMissingNumber.FindMissingNumber(num, 1, 5);
         assertEquals(4, missingNumber, "Ожидается, что недостающее число в первом массиве будет 4"); // Тест №1 Массив №1 с пропущенным числом 4
     }
 
     @Test
     public void testMissingNumber_SecondArray() {
-        int num1[] = {7,9,10,11,12};
+        int[] num1;
+        num1 = new int[]{7,9,10,11,12};
         int missingNumber = FindMissingNumber.FindMissingNumber(num1, 7, 12);
         assertEquals(8, missingNumber, "Ожидается, что недостающее число во втором массиве будет 8");  // Тест №2 Массив №2 с пропущенным числом 8
     }
