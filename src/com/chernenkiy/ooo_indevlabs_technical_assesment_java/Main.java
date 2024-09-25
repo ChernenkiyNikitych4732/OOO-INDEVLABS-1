609236886b089308d11cabe9acaedb1ca26137d0
Nikita <n.tchernenky@yandex.ru>
2024-09-25 21:34:16 +0200

    Техническое задание для компании ООО ИНДЕВЛАБС - 1

--- src/com/chernenkiy/ooo_indevlabs_technical_assesment_java/Main.java
+++ src/com/chernenkiy/ooo_indevlabs_technical_assesment_java/Main.java
@@ -1,6 +1,7 @@
 package com.chernenkiy.ooo_indevlabs_technical_assesment_java;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Техническое задание для компании ООО ИНДЕВЛАБС");
     }
